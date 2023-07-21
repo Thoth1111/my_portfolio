7bb64e15b8924ce31921d8b80b7c8ef7e6f1570a
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-21 20:52:33 +0300

    complete banner pics section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,10 +4,10 @@ import profile from '../assets/images/profile.jpg'
 
 const About = () => {
     return (
-        <div className="glassy rounded mx-auto mt-5 about">
-            <div className="mx-auto d-flex" style={{position: 'relative'}}>
-                <img src={profile} className="profile-pic" />
+        <div className="glassy mx-auto mt-5 about p-5">
+            <div className="px-auto d-flex justify-content-center pic-container">                
                 <img src={banner}  className="banner-pic"/>
+                <img src={profile} className="profile-pic" />
             </div>
         </div>
     );
