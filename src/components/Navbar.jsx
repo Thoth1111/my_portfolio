739d68d35630386f5fc7590349b38c8aeea72f55
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-21 20:51:48 +0300

    Fix linters & update Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-lg nav-menu glassy">
+        <nav className="navbar navbar-expand-lg nav-menu glassy" style={{borderRadius: '0px'}}>
             <div className="d-flex justify-content-center gap-5 container-fluid">
                 <a className="nav-font" href="#">About</a>
                 <a className="nav-font" href="#">Credentials</a>
