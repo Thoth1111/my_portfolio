795a7d083f264b9197893d95e5e00c4016420cfa
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-26 15:36:52 +0300

    Update Page component

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,25 +1,15 @@
-import React, { useEffect } from 'react';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/all';
+import React from 'react';
 import Navbar from './Navbar';
 import Profile from './Profile';
+import About from './About';
 
 const Page = () => {
-    useEffect(() => {
-        gsap.registerPlugin(ScrollTrigger);
-        gsap.to("progress", {
-            value: 100,
-                ScrollTrigger: {
-                scrub: 0.5,
-            },
-        });
-    }, []);
 
     return (
         <div className="main">
-            <progress max="100" value="0"></progress>
             <Navbar />
             <Profile />
+            <About />
         </div>
     );
 };
