9034834b99aaa89835be78e2674c88a69c7b6f5c
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-28 11:34:56 +0300

    Fix linters

--- server/App.js
+++ server/App.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import nodemailer from 'nodemailer';
-const app = express();
 import cors from 'cors';
 import dotenv from 'dotenv';
+
+const app = express();
 dotenv.config();
 const port = 5000;
 
@@ -10,46 +11,43 @@ app.use(cors());
 app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb' }));
 
-const sendEmail = ( name, email, message) => {
-  return new Promise((resolve, reject) => {
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.EMAIL,
-            pass: process.env.PASSWORD,
-        },
-    });
+const sendEmail = (name, email, message) => new Promise((resolve, reject) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
 
-    const mailOptions = {
-        from: `${email}`,
-        to: process.env.EMAIL,
-        subject: `Message from ${email}`,
-        text: `${message}.....${name}`,
-    };
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-            return reject({message: 'Something went wrong'});
-        } 
-        return resolve({message: 'Email sent successfully'});
-        });
-    });
-}
+  const mailOptions = {
+    from: `${email}`,
+    to: process.env.EMAIL,
+    subject: `Message from ${email}`,
+    text: `${message}.....${name}`,
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      return reject({ message: 'Something went wrong' });
+    }
+    return resolve({ message: 'Email sent successfully' });
+  });
+});
 
 app.get('/', (req, res) => {
-    sendEmail()
-    .then(response => res.send(response.message))
-    .catch(error => res.status(500).send(error.message));
+  sendEmail()
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
 });
 
 app.post('/send-email', (req, res) => {
-    const { name, email, message } = req.body;
-    sendEmail(name, email, message)
-    .then(response => res.send(response.message))
-    .catch(error => res.status(500).send(error.message));
+  const { name, email, message } = req.body;
+  sendEmail(name, email, message)
+    .then((response) => res.send(response.message))
+    .catch((error) => res.status(500).send(error.message));
 });
 
-app.listen(port, () => { 
-    console.log(`Server is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
