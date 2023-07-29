695d5abf50f7b99edb685bbf8e5f03794fe6d010
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-29 16:33:23 +0300

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,10 +50,10 @@ const Navbar = () => {
         :(
             <nav className="nav-menu py-2 px-4">
                 <div className="d-flex justify-content-center align-items-center gap-5 container-fluid">
-                    <a className="nav-font rounded" href="#about">About</a>
-                    {/* <a className="nav-font mb-0 pb-2" href="#credentials">Credentials</a> */}
-                    <a className="nav-font rounded p-2" href="#projects">Projects</a>
-                    <a className="nav-font rounded p-2" href="#contacts">Contacts</a>
+                    <a className="nav-font rounded px-1" href="#about">About</a>
+                    {/* <a className="nav-font rounded px-1" href="#credentials">Credentials</a> */}
+                    <a className="nav-font rounded px-1" href="#projects">Projects</a>
+                    <a className="nav-font rounded px-1" href="#contacts">Contacts</a>
                 </div>        
             </nav>
         )}
