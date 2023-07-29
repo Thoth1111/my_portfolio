ee00a43d6f315c947ba1775bf35b63c33d4bb00d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-29 04:15:30 +0300

    Update node server

--- server/App.js
+++ server/App.js
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 
 const app = express();
 dotenv.config();
-const port = 5000;
+const port = process.env.PORT ||5000;
 
 app.use(cors());
 app.use(express.json({ limit: '100mb' }));
