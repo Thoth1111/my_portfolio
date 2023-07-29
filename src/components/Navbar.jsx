852e58349031969a9370b99a6f47f492427cee73
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-29 16:29:52 +0300

    Style & Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
         :(
             <nav className="nav-menu py-2 px-4">
                 <div className="d-flex justify-content-center align-items-center gap-5 container-fluid">
-                    <a className="nav-font rounded p-2" href="#about">About</a>
+                    <a className="nav-font rounded" href="#about">About</a>
                     {/* <a className="nav-font mb-0 pb-2" href="#credentials">Credentials</a> */}
                     <a className="nav-font rounded p-2" href="#projects">Projects</a>
                     <a className="nav-font rounded p-2" href="#contacts">Contacts</a>
