5a619968a4eaebb54e66279f71dfbb5068e21edd
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-07-29 03:14:42 +0300

    Update Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,11 +16,16 @@ const Contact = ({ headingRef }) => {
                 email: email,
                 message: message
             })
-            .then(() => alert('Message sent successfully!'))
+            .then(() => {
+                alert('Message sent successfully!');
+                setName('');
+                setEmail('');
+                setMessage('');
+            })
             .catch(() => alert('An error occured while sending your message. Please try again later.'));
-            return;
+        } else {
+            alert('Please fill in all fields.');
         }
-        return alert('Please fill in all fields.');
     };
 
     const handleNameChange = (e) => {
@@ -36,19 +41,18 @@ const Contact = ({ headingRef }) => {
     };
 
     return (
-        <section className="d-flex flex-column align-items-center justify-content-center pt-5 contact">
-            <h1 className="heading my-5 pb-5" id="contacts" ref={headingRef}>Get in touch</h1>
-            <div className="d-flex justify-content-between align-items-center mb-5">
+        <section className="d-flex flex-column align-items-center justify-content-center pb-4 pt-5 contact">
+            <h1 className="heading" id="contacts" ref={headingRef}>Get in touch</h1>
+            <div className="contact-cont mb-5">
                 <div className="p-5 CTA">
-                    <h2 className="px-auto" style={{fontSize:'48px'}}>I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.</h2>
+                    <h2 className="px-auto py-auto">I'm always interested in hearing about new projects, so if you'd like to chat please get in touch.</h2>
                 </div>
-                <div className="form-entries p-0 mb-5">
-                    <form onSubmit={sendMail} className="d-flex flex-column gap-3 justify-content-center align-items-center">
-                        <input type="text" className="fill-area rounded" onChange={handleNameChange} id="fullname" name="name" maxLength="70" required placeholder="Full Name" />
-                        <input type="email" className="fill-area rounded" onChange={handleEmailChange} id="address" name="email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="Email Address" />
-                        <textarea rows="8" className="fill-area rounded" onChange={handleMessageChange} id="message" maxLength="500" required placeholder="Your message" />
-                        <input 
-                         className="rounded skill-btns p-3 mt-3 mb-5" id="post" type="submit" value="Email Me" style={{position: 'relative', right: '25%'}}/>
+                <div className="form-entries mb-5">
+                    <form onSubmit={sendMail} className="d-flex flex-column pt-4 mt-4 gap-3 justify-content-center align-items-end">
+                        <input type="text" className="fill-area rounded" onChange={handleNameChange} id="fullname" name="Your name" maxLength="70" required placeholder="Full Name" value={name}/>
+                        <input type="email" className="fill-area rounded" onChange={handleEmailChange} id="address" name="Your email" required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" placeholder="Email Address" value={email}/>
+                        <textarea rows="8" className="fill-area rounded" onChange={handleMessageChange} id="message" maxLength="500" required placeholder="Your message" value={message}/>
+                        <input type="submit" value="Email Me" className="rounded skill-btns p-3 mt-3 mb-5" id="post"/>
                     </form>   
                 </div>
             </div>
