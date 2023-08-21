06446458580645aecc178a2f4b4e7f734a1dc80d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-08-21 22:58:33 +0300

    Modify Skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
-import withIntersectionObserver from './withObserver';
+import React, { useState, useEffect, useRef} from 'react';
 import Carousel from './Carousel';
 
-const Skills = ({ headingRef }) => {
+const Skills = ({ darkMode }) => {
+    const [isVisible, setIsVisible] = useState(false);
+    const headingRef = useRef(null);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if(entry.isIntersecting) {
+                    setIsVisible(true);
+                }
+            });
+        });
+
+        if(headingRef.current) {
+            observer.observe(headingRef.current);
+        };
+
+        return () => {
+            if(headingRef.current) {
+                observer.unobserve(headingRef.current);
+            };
+        };
+    }, []);
+
     return (
         <>
         <section className="d-flex flex-column align-items-center justify-content-center pt-5">
-            <h3 className="heading" ref={headingRef} id="skills">Technical Skills</h3>            
+            <h3 ref={headingRef} className={`heading ${ isVisible ? 'animate' : ''}`} id="skills" style={ darkMode ? {color:'#d3d3d3'} : {}}>Technical Skills</h3>            
         </section>
         <Carousel />
         </>
     );
 };
 
-export default withIntersectionObserver(Skills);
+export default Skills;
